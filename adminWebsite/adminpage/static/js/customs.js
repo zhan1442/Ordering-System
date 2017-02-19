115b5a39829dba616f5aa2bda447779e917245d3
Zhizhe Zhang <zhan1442@purdue.edu>
2017-02-19 17:25:46 -0500

    2.19
    2.19 commit

--- adminWebsite/adminpage/static/js/customs.js
+++ adminWebsite/adminpage/static/js/customs.js
@@ -13,3 +13,15 @@ function getRadioVal(form, name) {
     }
     return val; // return value of checked radio or undefined if none checked
 }
+
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+    var $collapsing = $this.find('i').hasClass('glyphicon-chevron-down');
+    if(!$collapsing) {
+        $this.parents('.panel').find('.panel-body').slideUp(150);
+        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown(150);
+        $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    }
+})
